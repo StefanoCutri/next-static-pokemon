88b77a4d0581218475bb4b526858f7d1e2bf9220
Stefano Cutri <stefanocutristecco@gmail.com>
2023-09-12 18:41:56 -0300

    StaticPath to show dynamic routes

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,13 +1,40 @@
-import { Layout } from '@/components/layouts'
-import React from 'react'
+import { Layout } from "@/components/layouts";
+import { GetStaticProps, GetStaticPaths } from "next";
+import React from "react";
 
-const PokemonPage = () => {
+interface Props {
+  id: string;
+  name: string;
+}
+const PokemonPage = ({ id, name }: Props) => {
   return (
     <Layout>
-        <h1>Pokemon Page</h1>
+      <h1>
+        #{id} - {name}
+      </h1>
     </Layout>
-  )
-}
+  );
+};
 
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+  return {
+    paths: [
+      {
+        params: {
+          id: "1",
+        },
+      },
+    ],
+    fallback: "blocking",
+  };
+};
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  return {
+    props: {
+      id: "1",
+      name: "Pokemon",
+    },
+  };
+};
 
 export default PokemonPage;
