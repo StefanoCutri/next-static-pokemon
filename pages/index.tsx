7925ebd4799513535358c2948a9751cd837ae024
Stefano Cutri <stefanocutristecco@gmail.com>
2023-09-12 16:30:15 -0300

    Basic dark theme seted

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@nextui-org/react";
 import { NextPage } from "next";
 
 const HomePage : NextPage = ()=> {
   return (
     <>
       <h1>Hola mundo</h1>
+      <Button>Press me</Button>
     </>
   );
 };
