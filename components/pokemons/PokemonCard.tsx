970f81d65aba1dcb73a474b4d7de47c55b75b511
Stefano Cutri <stefanocutristecco@gmail.com>
2023-09-12 18:24:41 -0300

    Navigate to single page when click

--- components/pokemons/PokemonCard.tsx
+++ components/pokemons/PokemonCard.tsx
@@ -1,15 +1,23 @@
-import { SmallPokemon } from "@/interfaces";
 import { Card, Grid, Row, Text } from "@nextui-org/react";
+import { useRouter } from "next/router";
+
+import { SmallPokemon } from "@/interfaces";
 
 interface Props {
   pokemon: SmallPokemon;
 }
 
 export const PokemonCard = ({ pokemon }: Props) => {
-  const { id, name, img, url } = pokemon;
+  const { id, name, img } = pokemon;
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push(`/pokemon/${id}`)
+  }
+
   return (
     <Grid xs={6} sm={2} md={2} xl={1} key={id}>
-      <Card hoverable clickable>
+      <Card hoverable clickable onClick={onClick}>
         <Card.Body css={{ p: 1 }}>
           <Card.Image src={img} width="100%" height={140} />
         </Card.Body>
