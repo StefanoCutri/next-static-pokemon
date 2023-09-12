81eef03dbf9a02d9f0f6802260608c4757887163
Stefano Cutri <stefanocutristecco@gmail.com>
2023-09-12 18:08:14 -0300

    Showing full pokemon info

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import { NextPage, GetStaticProps } from "next";
+import { Card, Grid, Row, Text } from "@nextui-org/react";
+
 import { Layout } from "@/components/layouts";
 import { pokeApi } from "@/api";
 import { PokemonListResponse, SmallPokemon } from "@/interfaces";
@@ -12,23 +14,27 @@ const HomePage: NextPage<Props> = ({ pokemons }) => {
     <>
       <Layout title="Pokemon List">
         <h1>Hola mundo</h1>
-        <ul>
-          {pokemons.map(({ id, name }) => (
-            <li key={id}>
-              #{id} - {name}
-            </li>
+        <Grid.Container gap={2} justify="flex-start">
+          {pokemons.map(({ id, name, img }) => (
+            <Grid xs={6} sm={2} md={2} xl={1} key={id}>
+              <Card hoverable clickable>
+                <Card.Body css={{ p: 1 }}>
+                  <Card.Image src={img} width="100%" height={140} />
+                </Card.Body>
+                <Card.Footer>
+                  <Row justify="space-between">
+                    <Text transform="capitalize">{name}</Text>
+                    <Text>#{id}</Text>
+                  </Row>
+                </Card.Footer>
+              </Card>
+            </Grid>
           ))}
-        </ul>
+        </Grid.Container>
       </Layout>
     </>
   );
 };
-
-// You should use getStaticProps when:
-//- The data required to render the page is available at build time ahead of a user’s request.
-//- The data comes from a headless CMS.
-//- The data can be publicly cached (not user-specific).
-//- The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance.
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const { data } = await pokeApi.get<PokemonListResponse>("/pokemon?limit=151");
@@ -36,7 +42,9 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
   const pokemons: SmallPokemon[] = data.results.map((pokemon, i) => ({
     ...pokemon,
     id: i + 1,
-    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg",
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+      i + 1
+    }.svg`,
   }));
 
   console.log(pokemons);
