dffbe14d9daae65106f8ce44ea79c1f6fbcd887f
Stefano Cutri <stefanocutristecco@gmail.com>
2023-09-12 19:36:36 -0300

    Get pokemon info with dynamic routes

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,38 +1,41 @@
+import { pokeApi } from "@/api";
 import { Layout } from "@/components/layouts";
+import { Pokemon } from "@/interfaces";
 import { GetStaticProps, GetStaticPaths } from "next";
 import React from "react";
 
 interface Props {
-  id: string;
-  name: string;
+  pokemon: Pokemon;
 }
-const PokemonPage = ({ id, name }: Props) => {
+const PokemonPage = ({ pokemon }: Props) => {
+  console.log(pokemon);
+
   return (
     <Layout>
-      <h1>
-        #{id} - {name}
-      </h1>
+      <h1></h1>
     </Layout>
   );
 };
 
 export const getStaticPaths: GetStaticPaths = async (ctx) => {
+  const pokemonsTo151 = [...Array(151)].map((value, index) => `${index + 1}`);
+
   return {
-    paths: [
-      {
-        params: {
-          id: "1",
-        },
-      },
-    ],
-    fallback: "blocking",
+    paths: pokemonsTo151.map((id) => ({
+      params: { id },
+    })),
+    fallback: false,
   };
 };
-export const getStaticProps: GetStaticProps = async (ctx) => {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { id } = params as { id: string };
+
+  const { data } = await pokeApi.get<Pokemon>(`/pokemon/${id}`);
+console.log(data);
+
   return {
     props: {
-      id: "1",
-      name: "Pokemon",
+      pokemon: data
     },
   };
 };
