2a36502fd7e11f9ab5cbad367c17efbcb30b8134
Stefano Cutri <stefanocutristecco@gmail.com>
2023-09-12 17:07:26 -0300

    GetStaticProps template

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,33 @@
+import { NextPage, GetStaticProps } from "next";
 import { Layout } from "@/components/layouts";
-import { Button } from "@nextui-org/react";
-import { NextPage } from "next";
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
+  console.log(props);
+
   return (
     <>
-      <Layout title='Pokemon List'>
+      <Layout title="Pokemon List">
         <h1>Hola mundo</h1>
-        <Button>Press me</Button>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
       </Layout>
     </>
   );
+};
+
+// You should use getStaticProps when:
+//- The data required to render the page is available at build time ahead of a user’s request.
+//- The data comes from a headless CMS.
+//- The data can be publicly cached (not user-specific).
+//- The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance.
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  return {
+    props: {},
+  };
 };
 export default HomePage;
