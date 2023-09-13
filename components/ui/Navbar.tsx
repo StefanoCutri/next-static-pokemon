e41f972b5f41d647bf9c0eba4c62e09e09c4349f
Stefano Cutri <stefanocutristecco@gmail.com>
2023-09-13 18:52:55 -0300

    Add navigation links

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,5 +1,7 @@
 import { Spacer, Text, useTheme } from "@nextui-org/react";
+import NextLink from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 
 export const Navbar = () => {
   const { theme } = useTheme();
@@ -12,26 +14,34 @@ export const Navbar = () => {
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "start",
-        padding: "0px 20px",
+        padding: "0 20px",
         backgroundColor: theme?.colors.gray900.value,
       }}
     >
-      <Image
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
-        alt="App icon"
-        width={70}
-        height={70}
-      />
-
-      <Text color="white" h2>
-        P
-      </Text>
-      <Text color="white" h3>
-        okemon
-      </Text>
+      <NextLink href="/" passHref legacyBehavior>
+        <a style={{ display: "flex" }}>
+          <Image
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+            alt="App icon"
+            width={70}
+            height={70}
+          />
+        </a>
+      </NextLink>
+      <NextLink href="/" passHref legacyBehavior>
+        <a style={{ display: "flex" }}>
+          <Text color="white" h3>
+            Pókemon
+          </Text>
+        </a>
+      </NextLink>
 
       <Spacer css={{ flex: 1 }} />
-      <Text color="white">Favourites</Text>
+      <NextLink href="/favourites" passHref legacyBehavior>
+        <a style={{ display: "flex" }}>
+          <Text color="white">Favourites</Text>
+        </a>
+      </NextLink>
     </div>
   );
 };
---
+++ pages/favourites/index.jsx
@@ -0,0 +1,11 @@
+import {Layout} from '../../components/layouts/Layout'
+
+export const FavouritesPage = () => {
+  return (
+    <Layout>
+        <h1>Favourites</h1>
+    </Layout>
+  )
+}
+
+export default FavouritesPage
