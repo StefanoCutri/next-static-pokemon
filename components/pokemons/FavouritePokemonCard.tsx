0dffeaae07477966298a4064f5e235ac1a8de96a
Stefano Cutri <stefanocutristecco@gmail.com>
2023-09-14 10:51:38 -0300

    Navigate to favourite pokemon page

--- components/pokemons/FavouritePokemonCard.tsx
+++ components/pokemons/FavouritePokemonCard.tsx
@@ -1,9 +1,14 @@
 import { Card, Grid } from "@nextui-org/react"
+import { useRouter } from "next/router"
 
 export const FavouritePokemonCard = ({id}: {id: number}) => {
+    const router = useRouter();
+    const onFavouriteClicked = () => {
+        router.push(`/pokemon/${id}`)
+    }
   return (
     <Grid xs={6} sm={3} md={2} xl={1} key={id}>
-        <Card hoverable clickable css={{ padding: 10 }}>
+        <Card hoverable clickable css={{ padding: 10 }} onClick={onFavouriteClicked}>
           <Card.Image
             src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
             width="100%"
