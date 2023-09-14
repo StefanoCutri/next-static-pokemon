318bba31ac9bd58542b0e8d86390a70d6ffc0f40
Stefano Cutri <stefanocutristecco@gmail.com>
2023-09-14 12:29:04 -0300

    Meta tags to show image when sharing app

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -6,6 +6,8 @@ type Props = {
   title?: string;
 };
 
+const origin = (typeof(window) === 'undefined') ? '' : window.location.origin
+
 export const Layout = ({ children, title }: Props) => {
   return (
     <>
@@ -14,11 +16,27 @@ export const Layout = ({ children, title }: Props) => {
         <meta name="author" content="Stefano Cutri" />
         <meta name="description" content={`Pokemon info - ${title}`} />
         <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+        <meta
+          property="og:title"
+          content={`Pokemon info - ${title}`}
+        />
+        <meta
+          property="og:description"
+          content={`This is ${title} page.`}
+        />
+        <meta
+          property="og:image"
+          content={`${origin}/img/banner.png`}
+        />
       </Head>
       <Navbar />
-      <main style={{
-        padding: '0px 20px'
-      }}>{children}</main>
+      <main
+        style={{
+          padding: "0px 20px",
+        }}
+      >
+        {children}
+      </main>
     </>
   );
 };
