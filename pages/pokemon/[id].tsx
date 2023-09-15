ac41182bd32a0c7da7aca62d3abdcdb49032c9bc
Stefano Cutri <stefanocutristecco@gmail.com>
2023-09-15 00:21:19 -0300

    Load more pokemon pages and save it in runtime

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -111,15 +111,17 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
     paths: pokemonsTo151.map((id) => ({
       params: { id },
     })),
-    fallback: false,
+    fallback: 'blocking',
   };
 };
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params as { id: string };
 
+  const pokemon = await getPokemonInfo(id)
+
   return {
     props: {
-      pokemon: await getPokemonInfo(id),
+      pokemon,
     },
   };
 };
